bb319a7664f7213682007d5470f6ee4df8c72426
Leun Hammi <leuni070@gmail.com>
2026-06-19 11:58:50 +0530

    Add files via upload

---
+++ tresure hunt.py
@@ -0,0 +1,76 @@
+print('''  ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+ \_/__________________________________________________________________/''')
+ 
+print("TOUCH THE GOLD! LOSE YOUR SOUL!")
+print("\n WELCOME!!")
+name= input("\n what is ur name traveler?")
+print(f"\nWELCOME 'CAPTAIN {name}'")
+print(f"CHARACTERS:\n captain {name},\n parrot Cory,\n friend 1 Beck,\n friend 2 Chuck ")
+print("\n*you'r a gamer who is suddently pulled into the game,and dont know how to get out of here!... there is only one way! u have to find the tresure that is hidden for over 10 centuaries in a cursed island!")
+print("\n...DOOMS ISLES!...")
+print("\n*you see an island far away! and decides to land there!... ")
+print("\n*you reached the island and its beautiful!you decide to rest there for a while!...")
+print("\n* you eat some fruits and then the sun starts to set!...")
+print("\nBeck: captain lets prepare to rest here toningt!...")
+print("\n * after the sun sets! cory starts screaming like crazy,and yelling!'DANGER! DANGER!'")
+print("\n* and then the whole island becomes dark and creepy!, the trees die!, the waters surrounding the island turns red like blood!")
+print("\n Beck: this is... no this can't be!, it is the DOOMS ILSE!... no one goes back alive from here!")
+print("\n*suddenly pirate skeletons who are dead in the island emerges from the ground!")
+choice_1 = int(input("now you have 3 options!\n 1.run for your life inside the dark forest \n 2.fight the skeletons with your crew \n 3. run back to your ship and escape the island,\nwhat are u gonna choose?\n type 1 or 2 or 3... try anything else and AHRRR"))
+if choice_1==1:
+    print("\nyou escaped the skeletons!")
+elif choice_1==2:
+    print("\nthey never die and killed you and your crew! \nYOUR DEAD")
+    exit()
+else:
+    print("\nthe sea close to the island is deadly and you touched it!\n YOUR DEAD")
+    exit()
+print ("\nBeck: captain! where is cory!?")
+print("\n*you turn around and watch the skeletons pull down cory with them inside the groud!")
+print("cory: CAPTAIN! HELP! SAVE CORY !SAVE CORY!SAVE CO...")
+print("\n* you break down in tears and imagine the moments you guys spend with cory!")
+print("\nChuck: i've heard if one can reach the place where the treasure is kept, whatever they wish for comes true!")
+print("\n*you are determined to get there and wish for cory's revival!")
+print("\n*you started wandering in the forest")
+print("*a bright light flashed in a distance.\n Beck: be carefull captain! it might be a trap!")
+choice_2 = int(input("now you have 3 options!\n 1.stay where you are and wait for the light to fade.\n 2.rush in another way to avoid the light.\n 3. walk toward the light.\nwhat are u gonna choose?"))
+if choice_2==1:
+    print("the forest is dangerous! staying in one place in a cursed island is not a good idea! cursed creature attack you.\nYOUR DEAD")
+    exit()
+elif choice_2==2:
+    print("it is a slope! you cant see it because its dark and you slip and fall and die.\nYOUR DEAD")
+    exit()
+else:
+    print("the light is actually fireflies with the souls of the people died here before and it guides you through the forest!")
+print("\n*the fireflies guide you to a place totally different from the cursed island.\n* it looks like the curse of the island hasn't reached this place yet.\n* it's beautiful bright and pleasant to the eyes. \n*the you notice a cave which looks quite unusaual for a cave, and you plan to roll away the rock blocking the entrance of the cave.")
+print("\n*Beck: Capain, don't! legend has it there is a monster sleeping in a cave somewhere deep inside this island and once it's woken up there is no turning back and it can take on 100 people at once.")
+choice_3 = int(input("now you have 3 options!\n 1.Don't roll away the rock. 2.Roll away the rock and. 3. Shout,\"is there anybody inside?!?\"\n"))
+if choice_3==1:
+    print("\n nothing happens...")
+elif choice_3==2:
+    print("inside you don't find no monster but a large weapon stash! JACKPOT!!")
+else:
+    print("hearing this noise, many cursed beings run your way and kill you STUPID!!\n YOUR DEAD")
